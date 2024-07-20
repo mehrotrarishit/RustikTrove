6eb09bd662ab1795dfa0e1e4a1eba3be815a8f4b
Tanushreedew <tanushreedewangan26@gmail.com>
2024-07-20 20:47:10 +0530

    changed

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 const DB=process.env.DATABASE;
-mongoose.set('stricQuery',false)
+mongoose.set('strictQuery',false)
 mongoose.connect(DB).then(()=>console.log("database connected")).catch((error)=>console.log("error"+error.message));
